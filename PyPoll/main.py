5517a00cfb3effa6c4f93d1e77ac6bb0c236ef75
Zaki Malik Salim <zaki_klakla@yahoo.com>
2019-03-31 23:23:25 -0400

    final updates

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,37 +1,38 @@
 import csv
 import os
-csvfile_path= os.path.join("PyPoll\","election_data.csv")
+import collections
+csvfile_path= os.path.join("PyPoll","election_data.csv")
 with open(csvfile_path, newline="") as csvfile:
     csvreder=csv.reader(csvfile, delimiter=",")
     headers=next(csvreder)
     voters= []
-    canadidates=[]
+    canadidtes=[]
     candiate_name=[]
+    candiate_votes=0
     count_khan=0
     count_correy=0
     count_li=0
     for row in csvreder:
         voters.append(row[0])
         total_number= len(voters)
-        canadidate_name=row[2]
-        if candiate_name not in candiates:
-            canadidates.append(candiate_name)
-            candiate_votes[candiate_name]=0
-            candiate_votes=candiate_votes+1
-            percent= candiate_votes/total_number
+        candiate_name=(row[2])
 
-print("  Election Results ")
-print("-----------------------")
-print("Total Votes: " + str(total_number))
-for candiates in candiates_votes:
-    print(candiate_name, percent+"%", "("+candiate_votes+")")
 
-output_path = os.path.join("PyPoll/", "output", "new.csv")
-with open(output_path, 'w', newline='') as csvfile:
-    csvwriter = csv.writer(csvfile, delimiter=' ')
+    print("  Election Results ")
+    print("-----------------------")
+    print("Total Votes: " + str(total_number))
+    if candiate_name not in canadidtes:
+        canadidtes.append(candiate_name)
+        candiate_votes=len(candiate_name)
+        percent= candiate_votes/total_number
 
-output_path = os.path.join("PyPoll/", "new.csv")
-with open(output_path, 'w', newline='') as csvfile:
+        for candiates in canadidtes:
+            print(str(candiate_name),":", int(percent) ,"%", "(", float(candiate_votes),")")
+
+output_path = os.path.join("PyPoll", "new.csv")
+with open(output_path, newline='',mode='r+') as csvfile:
     csvwriter = csv.writer(csvfile, delimiter=' ')
     csvwriter.writerow([total_number])
-    csvwriter.writerow([candiate_name],[percent], [candiate_votes])
+    csvwriter.writerow([candiate_name])
+    csvwriter.writerow([percent])
+    csvwriter.writerow([candiate_votes])
